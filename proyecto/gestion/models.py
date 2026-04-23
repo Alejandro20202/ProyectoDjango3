bb01b9f248ce2ec5b723dbc46c3cdc72d1996a65
EstebanR26 <david.ordonezren@campusucc.edu.co>
2026-04-23 12:41:04 -0500

    cambios html

--- proyecto/gestion/models.py
+++ proyecto/gestion/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Modelo Autor
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    nacionalidad = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    biografia = models.TextField(blank=True, null=True)
+
+    def _str_(self):
+        return self.nombre
+
+
+# Modelo Libro
+class Libro(models.Model):
+    titulo = models.CharField(max_length=150)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=20, unique=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+
+    def _str_(self):
+        return self.titulo
